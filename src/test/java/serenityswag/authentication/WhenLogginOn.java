d722ef91b104c1d468d3749a71a749d229a033fc
JuanFe <juan.gomez164@udea.edu.co>
2022-07-25 09:15:26 -0500

    Ch3.3.2: Writing Your First Serenity BDD Test Case

--- src/test/java/serenityswag/authentication/WhenLogginOn.java
+++ src/test/java/serenityswag/authentication/WhenLogginOn.java
@@ -4,7 +4,10 @@ import net.serenitybdd.junit.runners.SerenityRunner;
 import net.thucydides.core.annotations.Managed;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+
+import static org.assertj.core.api.Assertions.assertThat;
 
 @RunWith(SerenityRunner.class)
 public class WhenLogginOn {
@@ -15,5 +18,21 @@ public class WhenLogginOn {
         @Test
         public void usersCanLogOnViaTheHomePage(){
         driver.get("https://www.saucedemo.com/");
-    }
+
+        //Login as standard user
+                driver.findElement(By.cssSelector("[data-test='username']")).sendKeys("standard_user");
+                driver.findElement(By.cssSelector("[data-test='password']")).sendKeys("secret_sauce");
+                driver.findElement(By.cssSelector("[data-test='login-button']")).click();
+
+        //Should see product catalog
+
+                assertThat(driver.findElement(By.cssSelector(".title")).getText())
+                        .isEqualToIgnoringCase("Products");
+                //.isEqualToIgnoringCase hace que sea indiferente de mayuscula-minuscula la comparacion
+
+
+               // driver.findElement(By.cssSelector("//*[@alt='Sauce Labs Backpack']")).click();
+
+        //Add to cart
+        }
 }
