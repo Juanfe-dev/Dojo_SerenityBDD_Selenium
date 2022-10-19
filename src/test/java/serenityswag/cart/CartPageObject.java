f81f3d47c3a8b1d85f059d505f1d1648d7eb2b36
Jgomez <jgomez@sistecredito.com>
2022-10-19 16:10:27 -0500

    Ch8.2.0: Using the FindBy Annotation in Serenity BDD

--- src/test/java/serenityswag/cart/CartPageObject.java
+++ src/test/java/serenityswag/cart/CartPageObject.java
@@ -1,13 +1,47 @@
 package serenityswag.cart;
 
 import net.serenitybdd.core.pages.PageObject;
+import net.serenitybdd.core.pages.WebElementFacade;
 import net.thucydides.core.annotations.DefaultUrl;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
 
+import java.util.ArrayList;
 import java.util.List;
 
 @DefaultUrl("https://www.saucedemo.com/cart.html")
 public class CartPageObject extends PageObject {
+
+    //@FindBy(css = "[data-test=checkout]")
+    @FindBy(id = "checkout")
+    WebElementFacade checkoutButton;
+
+    @FindBy(css = ".title")
+    WebElementFacade title;
+
+    @FindBy(className = "cart_item")
+    List<WebElementFacade> cartItemsElements;
+
+    public void checkout(){
+        checkoutButton.click();
+    }
+
+    public String getTitleText(){
+        return title.getText();
+    }
+
     public List<CartItem> items() {
-        return null;
+        List<CartItem> cartItems = new ArrayList<>();
+        for(WebElementFacade cartItemElement : cartItemsElements){
+            String name = cartItemElement.findBy(".inventory_item_name").getText();
+            String description = cartItemElement.findBy(".inventory_item_desc").getText();
+            Double price = priceFrom(cartItemElement.findBy(".inventory_item_price").getText());
+            cartItems.add(new CartItem(name, description, price));
+        }
+        return cartItems;
+    }
+
+    private Double priceFrom(String textValue) {
+        return Double.parseDouble(textValue.replace("$", ""));
     }
 }
