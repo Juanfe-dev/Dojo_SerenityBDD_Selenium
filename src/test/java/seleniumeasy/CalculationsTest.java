ac939487584df25ed5e59fc4241c1e1b83a38d5b
Jgomez <jgomez@sistecredito.com>
2022-11-09 11:52:23 -0500

    Chapter12.1.0: Using Test Data From A File

--- src/test/java/seleniumeasy/CalculationsTest.java
+++ src/test/java/seleniumeasy/CalculationsTest.java
@@ -6,6 +6,7 @@ import net.thucydides.junit.annotations.Concurrent;
 import net.thucydides.junit.annotations.Qualifier;
 import net.thucydides.junit.annotations.TestData;
 
+import net.thucydides.junit.annotations.UseTestDataFrom;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.openqa.selenium.WebDriver;
@@ -18,23 +19,49 @@ import static org.assertj.core.api.Assertions.assertThat;
 
 @RunWith(SerenityParameterizedRunner.class)
 @Concurrent
+@UseTestDataFrom("test-data/calculations.csv")
 public class CalculationsTest {
 
+    private  String a;
+    private  String b;
+    private  String total;
+    /*
+    //Used in Chapter 12.0.0
+    //Commented for Chapter 12.1.0
     private final String a;
     private final String b;
     private final String total;
+    */
 
     @Managed(driver = "chrome", options = "--headless")
     WebDriver driver;
 
+    @Qualifier
+    public String qualifier() {return a +" + "+ b +" should equal "+ total;}
+
+    TwoInputFieldForm twoInputFieldForm;
+    @Test
+    public void shouldCalculateTheCorrectTotal(){
+        twoInputFieldForm.open();
+        twoInputFieldForm.enterA(a);
+        twoInputFieldForm.enterB(b);
+        twoInputFieldForm.getTotal();
+        assertThat(twoInputFieldForm.displayedTotal()).isEqualTo(total);
+    }
+}
+
+    /*
+    //Used in Chapter 12.0.0
+    //Commented for Chapter 12.1.0
     public CalculationsTest(String a, String b, String total) {
         this.a = a;
         this.b = b;
         this.total = total;
-    }
+    }*/
 
-    TwoInputFieldForm twoInputFieldForm;
-
+    /*
+    //Used in Chapter 12.0.0
+    //Commented for Chapter 12.1.0
     @TestData(columnNames = "A, B, Total")
     public static Collection<Object[]> testData() {
         return Arrays.asList(
@@ -46,17 +73,6 @@ public class CalculationsTest {
                     {"10000","20000","30000"},
             }
         );
-    }
-    @Qualifier
-    public String qualifier() {return a +" + "+ b +" should equal "+ total;}
+    }*/
 
-    @Test
-    public void shouldCalculateTheCorrectTotal(){
-        twoInputFieldForm.open();
-        twoInputFieldForm.enterA(a);
-        twoInputFieldForm.enterB(b);
-        twoInputFieldForm.getTotal();
-        assertThat(twoInputFieldForm.displayedTotal()).isEqualTo(total);
-    }
 
-}
